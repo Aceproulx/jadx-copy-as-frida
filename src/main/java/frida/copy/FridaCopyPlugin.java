4bace72b117efeb3a82a52eb2256566081b21fd9
Aceproulx <masangamike07@gmail.com>
2026-07-21 00:51:51 +0700

    fix: bracket notation for method calls, real param names, send(), Java.perform wrapper

--- src/main/java/frida/copy/FridaCopyPlugin.java
+++ src/main/java/frida/copy/FridaCopyPlugin.java
@@ -10,7 +10,9 @@ import jadx.api.plugins.JadxPluginInfoBuilder;
 import jadx.api.plugins.gui.JadxGuiContext;
 import jadx.api.metadata.ICodeAnnotation;
 import jadx.api.metadata.ICodeNodeRef;
+import jadx.api.metadata.annotations.VarNode;
 import jadx.core.dex.info.MethodInfo;
+import jadx.core.dex.instructions.args.ArgType;
 import jadx.core.dex.nodes.ClassNode;
 import jadx.core.dex.nodes.MethodNode;
 import java.util.List;
@@ -65,213 +67,40 @@ public class FridaCopyPlugin implements JadxPlugin {
         if (mthNode == null) return false;
         ClassNode cls = mthNode.getDeclaringClass();
         if (cls == null) return false;
-        MethodInfo info = mthNode.getMethodInfo();
+        String shortId = mthNode.getMethodInfo().getShortId();
         for (MethodNode other : cls.getMethods()) {
-            if (other.getMethodInfo() != info && other.getMethodInfo().isOverloadedBy(info)) {
+            if (!other.getMethodInfo().getShortId().equals(shortId)
+                    && other.getMethodInfo().getName().equals(mth.getName())) {
                 return true;
             }
         }
         return false;
     }
 
-    // ---- Script generation ----
-
-    private String buildScript(JavaMethod mth, ScriptType type) {
-        String cls = mth.getDeclaringClass().getFullName();
-        String name = mth.getName();
-        List<String> argTypes = mth.getArguments().stream()
-                .map(t -> toFridaType(t.toString()))
+    private List<String> getArgNames(JavaMethod mth) {
+        MethodNode mthNode = mth.getMethodNode();
+        if (mthNode == null) {
+            return mth.getArguments().stream()
+                    .map(t -> fallbackName(t.toString()))
+                    .collect(Collectors.toList());
+        }
+        List<VarNode> argNodes = mthNode.collectArgNodes();
+        if (!argNodes.isEmpty()) {
+            return argNodes.stream()
+                    .map(VarNode::getName)
+                    .collect(Collectors.toList());
+        }
+        return mth.getArguments().stream()
+                .map(t -> fallbackName(t.toString()))
                 .collect(Collectors.toList());
-
-        if (type == ScriptType.OVERLOADS || isOverloaded(mth)) {
-            return buildOverloaded(cls, name, type);
-        }
-
-        String decl = buildParamDecl(argTypes);
-        String call = buildArgList(argTypes.size());
-
-        switch (type) {
-            case LOG: return genLog(cls, name, decl, call, argTypes);
-            case RETURN_TRUE: return genReturn(cls, name, decl, "true");
-            case RETURN_FALSE: return genReturn(cls, name, decl, "false");
-            case TRACE: return genTrace(cls, name, decl, call);
-            default: return "";
-        }
     }
 
-    // ---- Single-overload scripts ----
-
-    private String genLog(String cls, String name, String decl, String call, List<String> argTypes) {
-        StringBuilder logParts = new StringBuilder();
-        for (int i = 0; i < argTypes.size(); i++) {
-            if (i > 0) logParts.append(" + \", \" + ");
-            logParts.append("\"").append(argTypes.get(i)).append(i).append("=\" + ").append(argTypes.get(i)).append(i);
-        }
-        String logExpr = logParts.length() > 0 ? logParts.toString() : "\"\"";
-
-        return String.format(
-            "Java.perform(function() {\n" +
-            "  var %s = Java.use(\"%s\");\n" +
-            "  %s.%s.implementation = function(%s) {\n" +
-            "    console.log(\"[frida-copy] %s.%s(\" + %s + \")\");\n" +
-            "    var ret = this.%s(%s);\n" +
-            "    console.log(\"[frida-copy] %s.%s => \" + ret);\n" +
-            "    return ret;\n" +
-            "  };\n" +
-            "  console.log(\"[frida-copy] Hooked %s.%s\");\n" +
-            "});\n",
-            var(cls), cls,
-            var(cls), name, decl,
-            cls, name, logExpr,
-            name, call,
-            cls, name,
-            cls, name
-        );
-    }
-
-    private String genReturn(String cls, String name, String decl, String retVal) {
-        return String.format(
-            "Java.perform(function() {\n" +
-            "  var %s = Java.use(\"%s\");\n" +
-            "  %s.%s.implementation = function(%s) {\n" +
-            "    console.log(\"[frida-copy] %s.%s => %s bypass\");\n" +
-            "    return %s;\n" +
-            "  };\n" +
-            "  console.log(\"[frida-copy] %s.%s => %s bypass\");\n" +
-            "});\n",
-            var(cls), cls,
-            var(cls), name, decl,
-            cls, name, retVal,
-            retVal,
-            cls, name, retVal
-        );
-    }
-
-    private String genTrace(String cls, String name, String decl, String call) {
-        return String.format(
-            "Java.perform(function() {\n" +
-            "  var %s = Java.use(\"%s\");\n" +
-            "  %s.%s.implementation = function(%s) {\n" +
-            "    console.log(\"[frida-copy] %s.%s()\");\n" +
-            "    console.log(Java.use(\"android.util.Log\").getStackTraceString(\n" +
-            "      Java.use(\"java.lang.Throwable\").$new()));\n" +
-            "    return this.%s(%s);\n" +
-            "  };\n" +
-            "  console.log(\"[frida-copy] Traced %s.%s\");\n" +
-            "});\n",
-            var(cls), cls,
-            var(cls), name, decl,
-            cls, name,
-            name, call,
-            cls, name
-        );
-    }
-
-    // ---- Overloaded scripts ----
-
-    private String buildOverloaded(String cls, String name, ScriptType type) {
-        switch (type) {
-            case LOG:
-                return String.format(
-                    "Java.perform(function() {\n" +
-                    "  var %s = Java.use(\"%s\");\n" +
-                    "  %s.%s.overloads.forEach(function(overload) {\n" +
-                    "    overload.implementation = function() {\n" +
-                    "      var args = Array.prototype.slice.call(arguments);\n" +
-                    "      console.log(\"[frida-copy] %s.%s(\" + JSON.stringify(args) + \")\");\n" +
-                    "      var ret = overload.apply(this, arguments);\n" +
-                    "      console.log(\"[frida-copy] %s.%s => \" + ret);\n" +
-                    "      return ret;\n" +
-                    "    };\n" +
-                    "  });\n" +
-                    "  console.log(\"[frida-copy] Hooked %s.%s (all overloads)\");\n" +
-                    "});\n",
-                    var(cls), cls,
-                    var(cls), name,
-                    cls, name,
-                    cls, name,
-                    cls, name
-                );
-            case RETURN_TRUE:
-                return overloadedReturn(cls, name, "true");
-            case RETURN_FALSE:
-                return overloadedReturn(cls, name, "false");
-            case TRACE:
-                return String.format(
-                    "Java.perform(function() {\n" +
-                    "  var %s = Java.use(\"%s\");\n" +
-                    "  %s.%s.overloads.forEach(function(overload) {\n" +
-                    "    overload.implementation = function() {\n" +
-                    "      console.log(\"[frida-copy] %s.%s() (overloaded)\");\n" +
-                    "      console.log(Java.use(\"android.util.Log\").getStackTraceString(\n" +
-                    "        Java.use(\"java.lang.Throwable\").$new()));\n" +
-                    "      return overload.apply(this, arguments);\n" +
-                    "    };\n" +
-                    "  });\n" +
-                    "  console.log(\"[frida-copy] Traced %s.%s (all overloads)\");\n" +
-                    "});\n",
-                    var(cls), cls,
-                    var(cls), name,
-                    cls, name,
-                    cls, name
-                );
-            default:
-                return "";
-        }
-    }
-
-    private String overloadedReturn(String cls, String name, String retVal) {
-        return String.format(
-            "Java.perform(function() {\n" +
-            "  var %s = Java.use(\"%s\");\n" +
-            "  %s.%s.overloads.forEach(function(overload) {\n" +
-            "    overload.implementation = function() {\n" +
-            "      console.log(\"[frida-copy] %s.%s => %s bypass (overloaded)\");\n" +
-            "      return %s;\n" +
-            "    };\n" +
-            "  });\n" +
-            "  console.log(\"[frida-copy] %s.%s => %s bypass (all overloads)\");\n" +
-            "});\n",
-            var(cls), cls,
-            var(cls), name,
-            cls, name, retVal,
-            retVal,
-            cls, name, retVal
-        );
-    }
-
-    // ---- Helpers ----
-
-    private static String buildParamDecl(List<String> argTypes) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < argTypes.size(); i++) {
-            if (i > 0) sb.append(", ");
-            sb.append(argTypes.get(i)).append(i);
-        }
-        return sb.toString();
-    }
-
-    private static String buildArgList(int count) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < count; i++) {
-            if (i > 0) sb.append(", ");
-            sb.append("_").append(i);
-        }
-        return sb.toString();
-    }
-
-    private static String var(String fullName) {
-        int dot = fullName.lastIndexOf('.');
-        String s = dot >= 0 ? fullName.substring(dot + 1) : fullName;
-        return s.replace("$", "_");
-    }
-
-    private static String toFridaType(String type) {
+    private String fallbackName(String type) {
         if (type == null || type.isEmpty()) return "arg";
         switch (type) {
-            case "boolean": return "b";
-            case "byte":    return "by";
-            case "char":    return "c";
+            case "boolean": return "bool";
+            case "byte":    return "byte";
+            case "char":    return "ch";
             case "short":   return "sh";
             case "int":     return "i";
             case "long":    return "l";
@@ -280,8 +109,206 @@ public class FridaCopyPlugin implements JadxPlugin {
             default:
                 if (type.startsWith("[")) return "arr";
                 int lastDot = type.lastIndexOf('.');
-                return lastDot >= 0 ? type.substring(lastDot + 1).toLowerCase() : "obj";
+                return lastDot >= 0
+                        ? type.substring(lastDot + 1).toLowerCase()
+                        : "obj";
         }
+    }
+
+    // ---- Script generation ----
+
+    private String buildScript(JavaMethod mth, ScriptType type) {
+        MethodNode mthNode = mth.getMethodNode();
+        MethodInfo info = mthNode != null ? mthNode.getMethodInfo() : null;
+        String rawCls = mth.getDeclaringClass().getRawName();
+        String clsAlias = mth.getDeclaringClass().getName();
+        String varName = clsAlias.replace("$", "_");
+        boolean isConstructor = info != null && info.isConstructor();
+        String methodName = isConstructor ? "$init" : info != null ? info.getName() : mth.getName();
+        String aliasName = isConstructor ? "$init" : mth.getName();
+        boolean overloaded = isOverloaded(mth);
+
+        String overloadSig = "";
+        if (overloaded) {
+            overloadSig = ".overload(" + mth.getArguments().stream()
+                    .map(t -> "'" + parseArgType(t) + "'")
+                    .collect(Collectors.joining(", ")) + ")";
+        }
+
+        List<String> argNames = getArgNames(mth);
+        String argList = String.join(", ", argNames);
+
+        boolean isVoid = !isConstructor && info != null && info.getReturnType() == ArgType.VOID;
+
+        String body;
+        if (type == ScriptType.OVERLOADS || overloaded) {
+            body = buildOverloaded(mth, rawCls, clsAlias, varName, methodName, aliasName, type);
+        } else {
+            switch (type) {
+                case LOG:
+                    body = isVoid
+                            ? genLogVoid(varName, rawCls, clsAlias, methodName, aliasName, overloadSig, argList, argNames)
+                            : genLogReturn(varName, rawCls, clsAlias, methodName, aliasName, overloadSig, argList, argNames);
+                    break;
+                case RETURN_TRUE:  body = genReturn(varName, rawCls, clsAlias, methodName, aliasName, overloadSig, argList, "true"); break;
+                case RETURN_FALSE: body = genReturn(varName, rawCls, clsAlias, methodName, aliasName, overloadSig, argList, "false"); break;
+                case TRACE:        body = genTrace(varName, rawCls, clsAlias, methodName, aliasName, overloadSig, argList, argNames); break;
+                default: body = "";
+            }
+        }
+        return "Java.perform(function() {\n" + body + "});\n";
+    }
+
+    // ---- Non-overloaded scripts ----
+
+    private String genLogReturn(String var, String rawCls, String clsAlias, String name, String alias,
+                                 String overloadSig, String argList, List<String> argNames) {
+        String logCall = buildLogCall(alias, name, argNames);
+        return String.format(
+            "var %s = Java.use(\"%s\");\n" +
+            "%s[\"%s\"]%s = function (%s) {\n" +
+            "  send(`%s.%s is called: %s`);\n" +
+            "  var ret = this[\"%s\"](%s);\n" +
+            "  send(`%s.%s result=${ret}`);\n" +
+            "  return ret;\n" +
+            "};\n",
+            var, rawCls,
+            var, name, overloadSig, argList,
+            clsAlias, name, logCall,
+            name, argList,
+            clsAlias, name
+        );
+    }
+
+    private String genLogVoid(String var, String rawCls, String clsAlias, String name, String alias,
+                               String overloadSig, String argList, List<String> argNames) {
+        String logCall = buildLogCall(alias, name, argNames);
+        return String.format(
+            "var %s = Java.use(\"%s\");\n" +
+            "%s[\"%s\"]%s = function (%s) {\n" +
+            "  send(`%s.%s is called: %s`);\n" +
+            "  this[\"%s\"](%s);\n" +
+            "};\n",
+            var, rawCls,
+            var, name, overloadSig, argList,
+            clsAlias, name, logCall,
+            name, argList
+        );
+    }
+
+    private String genReturn(String var, String rawCls, String clsAlias, String name, String alias,
+                              String overloadSig, String argList, String retVal) {
+        return String.format(
+            "var %s = Java.use(\"%s\");\n" +
+            "%s[\"%s\"]%s = function (%s) {\n" +
+            "  send(`%s.%s => %s bypass`);\n" +
+            "  return %s;\n" +
+            "};\n",
+            var, rawCls,
+            var, name, overloadSig, argList,
+            clsAlias, name, retVal,
+            retVal
+        );
+    }
+
+    private String genTrace(String var, String rawCls, String clsAlias, String name, String alias,
+                             String overloadSig, String argList, List<String> argNames) {
+        String logCall = buildLogCall(alias, name, argNames);
+        return String.format(
+            "var %s = Java.use(\"%s\");\n" +
+            "%s[\"%s\"]%s = function (%s) {\n" +
+            "  send(`%s.%s is called: %s`);\n" +
+            "  send(Java.use(\"android.util.Log\").getStackTraceString(\n" +
+            "    Java.use(\"java.lang.Throwable\").$new()));\n" +
+            "  return this[\"%s\"](%s);\n" +
+            "};\n",
+            var, rawCls,
+            var, name, overloadSig, argList,
+            clsAlias, name, logCall,
+            name, argList
+        );
+    }
+
+    // ---- Overloaded scripts ----
+
+    private String buildOverloaded(JavaMethod mth, String rawCls, String clsAlias, String varName,
+                                    String name, String alias, ScriptType type) {
+        String overloadTypes = mth.getArguments().stream()
+                .map(t -> "'" + parseArgType(t) + "'")
+                .collect(Collectors.joining(", "));
+
+        switch (type) {
+            case LOG:
+                return String.format(
+                    "var %s = Java.use(\"%s\");\n" +
+                    "%s[\"%s\"].overloads.forEach(function(overload) {\n" +
+                    "  overload.implementation = function() {\n" +
+                    "    var args = Array.prototype.slice.call(arguments);\n" +
+                    "    send(`%s.%s called with ${JSON.stringify(args)}`);\n" +
+                    "    var ret = overload.apply(this, arguments);\n" +
+                    "    send(`%s.%s result=${ret}`);\n" +
+                    "    return ret;\n" +
+                    "  };\n" +
+                    "});\n",
+                    varName, rawCls,
+                    varName, name,
+                    clsAlias, name,
+                    clsAlias, name
+                );
+            case RETURN_TRUE:
+                return overloadedReturn(varName, rawCls, clsAlias, name, "true");
+            case RETURN_FALSE:
+                return overloadedReturn(varName, rawCls, clsAlias, name, "false");
+            case TRACE:
+                return String.format(
+                    "var %s = Java.use(\"%s\");\n" +
+                    "%s[\"%s\"].overloads.forEach(function(overload) {\n" +
+                    "  overload.implementation = function() {\n" +
+                    "    send(`%s.%s called (overloaded)`);\n" +
+                    "    send(Java.use(\"android.util.Log\").getStackTraceString(\n" +
+                    "      Java.use(\"java.lang.Throwable\").$new()));\n" +
+                    "    return overload.apply(this, arguments);\n" +
+                    "  };\n" +
+                    "});\n",
+                    varName, rawCls,
+                    varName, name,
+                    clsAlias, name
+                );
+            default:
+                return "";
+        }
+    }
+
+    private String overloadedReturn(String var, String rawCls, String clsAlias, String name, String retVal) {
+        return String.format(
+            "var %s = Java.use(\"%s\");\n" +
+            "%s[\"%s\"].overloads.forEach(function(overload) {\n" +
+            "  overload.implementation = function() {\n" +
+            "    send(`%s.%s => %s bypass (overloaded)`);\n" +
+            "    return %s;\n" +
+            "  };\n" +
+            "});\n",
+            var, rawCls,
+            var, name,
+            clsAlias, name, retVal,
+            retVal
+        );
+    }
+
+    // ---- Helpers ----
+
+    private static String buildLogCall(String clsAlias, String name, List<String> argNames) {
+        if (argNames.isEmpty()) return "";
+        return argNames.stream()
+                .map(n -> n + "=${" + n + "}")
+                .collect(Collectors.joining(", "));
+    }
+
+    private static String parseArgType(ArgType type) {
+        if (type.isArray()) {
+            return jadx.core.codegen.TypeGen.signature(type).replace("/", ".");
+        }
+        return type.toString();
     }
 
     private enum ScriptType {
